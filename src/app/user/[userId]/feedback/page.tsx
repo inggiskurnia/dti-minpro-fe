99e0b0ad21bd536b06b5337b3e2c18fbba8fa93a
Inggis Kurnia Trisiawan <inggiskurnia32@gmail.com>
2024-12-15 18:17:27 +0700

    feat:event review and feedback

--- src/app/user/[userId]/feedback/page.tsx
+++ src/app/user/[userId]/feedback/page.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+import Footer from "@/components/Footer";
+import Navbar from "@/components/Navbar";
 import { useState } from "react";
 
 const EventFeedback = () => {
@@ -10,25 +12,34 @@ const EventFeedback = () => {
   };
 
   return (
-    <div className="bg-white p-6 rounded-lg shadow-md max-w-md mx-auto">
-      <h2 className="text-2xl font-bold mb-4">Event Feedback</h2>
+    <div className="flex flex-col min-h-screen">
+      <Navbar></Navbar>
+      <div className="flex-grow flex justify-center pt-24 pb-20">
+        <div className="bg-white p-6 rounded-lg shadow-md w-1/3 mx-auto">
+          <h2 className="text-2xl font-bold mb-4 text-gray-700">
+            Event Feedback
+          </h2>
 
-      <textarea
-        value={feedback}
-        onChange={handleFeedbackChange}
-        placeholder="Provide your feedback here..."
-        rows={6}
-        className="w-full p-4 border rounded-lg text-gray-700 focus:outline-none focus:ring-2 focus:ring-blue-500"
-      />
+          <textarea
+            value={feedback}
+            onChange={handleFeedbackChange}
+            placeholder="Provide your feedback here..."
+            rows={6}
+            className="w-full p-4 border rounded-lg text-gray-700 focus:outline-none focus:ring-2 focus:ring-blue-500"
+          />
 
-      <div className="mt-4 flex justify-end">
-        <button
-          className="bg-green-500 text-white px-6 py-2 rounded-lg hover:bg-green-600 focus:outline-none"
-          disabled={!feedback}
-        >
-          Submit Feedback
-        </button>
+          <div className="mt-4 flex justify-end">
+            <button
+              className="bg-green-500 text-white px-6 py-2 rounded-lg hover:bg-green-600 focus:outline-none"
+              disabled={!feedback}
+            >
+              Submit Feedback
+            </button>
+          </div>
+        </div>
       </div>
+
+      <Footer></Footer>
     </div>
   );
 };
--- src/app/user/[userId]/review/page.tsx
+++ src/app/user/[userId]/review/page.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+import Footer from "@/components/Footer";
+import Navbar from "@/components/Navbar";
 import { useState } from "react";
 import { FaStar, FaRegStar } from "react-icons/fa";
 
@@ -12,41 +14,50 @@ const EventReview = () => {
   };
 
   return (
-    <div className="bg-white p-6 rounded-lg shadow-md max-w-md mx-auto">
-      <h2 className="text-2xl font-bold mb-4">Event Review</h2>
+    <div className="flex flex-col min-h-screen">
+      <Navbar />
 
-      <div className="flex items-center mb-4">
-        {[1, 2, 3, 4, 5].map((star) => (
-          <div
-            key={star}
-            className="cursor-pointer"
-            onClick={() => handleStarClick(star)}
-          >
-            {star <= rating ? (
-              <FaStar className="text-yellow-500 text-3xl" />
-            ) : (
-              <FaRegStar className="text-gray-400 text-3xl" />
-            )}
+      <div className="flex-grow flex justify-center pt-24 pb-20">
+        <div className="bg-white p-6 rounded-lg shadow-md mx-auto w-1/3">
+          <h2 className="text-2xl font-bold text-gray-700 mb-4">
+            Event Review
+          </h2>
+
+          <div className="flex items-center mb-4">
+            {[1, 2, 3, 4, 5].map((star) => (
+              <div
+                key={star}
+                className="cursor-pointer"
+                onClick={() => handleStarClick(star)}
+              >
+                {star <= rating ? (
+                  <FaStar className="text-yellow-500 text-3xl" />
+                ) : (
+                  <FaRegStar className="text-gray-400 text-3xl" />
+                )}
+              </div>
+            ))}
           </div>
-        ))}
-      </div>
 
-      <textarea
-        value={review}
-        onChange={(e) => setReview(e.target.value)}
-        placeholder="Write your review..."
-        rows={4}
-        className="w-full p-4 border rounded-lg text-gray-700 focus:outline-none focus:ring-2 focus:ring-blue-500"
-      />
+          <textarea
+            value={review}
+            onChange={(e) => setReview(e.target.value)}
+            placeholder="Write your review..."
+            rows={4}
+            className="w-full p-4 border rounded-lg text-gray-700 focus:outline-none focus:ring-2 focus:ring-blue-500"
+          />
 
-      <div className="mt-4 flex justify-end">
-        <button
-          className="bg-blue-500 text-white px-6 py-2 rounded-lg hover:bg-blue-600 focus:outline-none"
-          disabled={!review || rating === 0}
-        >
-          Submit Review
-        </button>
+          <div className="mt-4 flex justify-end">
+            <button
+              className="bg-blue-500 text-white px-6 py-2 rounded-lg hover:bg-blue-600 focus:outline-none"
+              disabled={!review || rating === 0}
+            >
+              Submit Review
+            </button>
+          </div>
+        </div>
       </div>
+      <Footer />
     </div>
   );
 };
